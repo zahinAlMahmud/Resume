edbbc7c09e8d8351c66536d455361d39f29a818b
zahinAlMahmud <zahinmahmud327@gmail.com>
2025-10-12 15:04:40 +0600

    Deploy latest build

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import  Navbar from './components/Navbar/Navbar.jsx';
-import  MainContent from './components/MainContent/MainContent.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
+import MainContent from './components/MainContent/MainContent.jsx';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
-function App (){
+function App() {
   return (
-  <div className="d-flex">
-      <Navbar />
-      <div className="flex-grow-1">
+    <div className="d-flex flex-column flex-md-row min-vh-100">
+      {/* Sidebar/Navbar */}
+      <div className="flex-shrink-0 bg-dark text-white" style={{ width: '100%', maxWidth: '250px' }}>
+        <Navbar />
+      </div>
+
+      {/* Main Content */}
+      <div className="flex-grow-1 p-3">
         <MainContent />
       </div>
     </div>
-
-  )
-
+  );
 }
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // or vue plugin if using Vue
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/Resume/',   // important for GitHub Pages
+  plugins: [react()]
 })
